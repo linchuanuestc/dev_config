adb88e2294520d461e893ffa7265f3db904ce621
linchuanuestc <linchuanuestc@gmail.com>
2020-12-31 18:01:26 +0800

    add try times

--- tmux_install.sh
+++ tmux_install.sh
@@ -4,7 +4,7 @@ export HOME="/home/linchuan"
 #install tmux3.1.b 这个版本可以控制颜色, https://github.com/tmux/tmux/wiki/Installing, 不推荐使用源码安装
 #重试三次
 n=0
-until [ "$n" -ge 2 ]
+until [ "$n" -ge 5 ]
 do
    sudo yum install --randomwait=1 -v http://galaxy4.net/repo/galaxy4-release-7-current.noarch.rpm && break  # substitute your command here
    n=$((n+1))
@@ -14,7 +14,7 @@ done
 sudo yum remove tmux
 
 n=0
-until [ "$n" -ge 2 ]
+until [ "$n" -ge 5 ]
 do
     sudo yum install --randomwait=1 -v tmux && break  # substitute your command here
    n=$((n+1))
