27c45286f53c2fcb8802bf392dca9c3cb6e89a2f
linchuanuestc <linchuanuestc@gmail.com>
2022-03-22 17:47:51 +0800

    add Ycm

---
+++ .vim_YCM_GO/pack/plugins/start/YouCompleteMe/third_party/ycmd/third_party/git.sh
@@ -0,0 +1,10 @@
+git clone git@git.zhlh6.cn:defnull/bottle
+
+git clone git@git.zhlh6.cn:davidhalter/jedi jedi_deps/jedi
+
+git clone git@git.zhlh6.cn:numpy/numpydoc jedi_deps/numpydoc
+
+git clone git@git.zhlh6.cn:davidhalter/parso jedi_deps/parso
+
+git clone git@git.zhlh6.cn:gorakhargosh/watchdog watchdog_deps/watchdog
+git clone https://bitbucket.org/mrabarnett/mrab-regex.git
